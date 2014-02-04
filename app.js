6aaec13c0ed1b6e14cb8388bc37f462003904e4a
sunanda <sunandach88@gmail.com>
2014-02-04 14:30:40 +0530

    database is made configurable(mongodb or tingodb)

--- app.js
+++ app.js
@@ -15,60 +15,64 @@ var express = require('express')
     , db = dbEngine.getDB();
 
 db.open(function(err, db){
-	var dbOptions = {db : db , engine : dbEngine.engine};
-	var expressConfig = require('./config/express');
-	expressConfig(app, express, path, __dirname, passport, config);
+	if(!err){
+		var dbOptions = {db : db , engine : dbEngine.engine};
+		var expressConfig = require('./config/express');
+		expressConfig(app, express, path, __dirname, passport, config);
 
-	var passportConf = require('./config/passport');
-	passportConf(passport, dbOptions, config);
+		var passportConf = require('./config/passport');
+		passportConf(passport, dbOptions, config);
 
-	var auth = require('./config/auth')(passport, express);
+		var auth = require('./config/auth')(passport, express);
 
-	var reqmap = require('./config/reqmap');
-	reqmap(app, dbOptions, passport, auth);
+		var reqmap = require('./config/reqmap');
+		reqmap(app, dbOptions, passport, auth);
 
-	var httpServer = http.createServer(app);
+		var httpServer = http.createServer(app);
 
-	httpServer.listen(config.app.port, config.app.hostname, function(req,
-			res, next) {
-		console.log('Http server on port : ' + config.app.port);
-		var watchmen = new WatchMen();
-		watchmen.start();
-		
-		watchmen.on('service_error', function(host, state) {
-			  if (state.prev_state.status === 'success' && host.config.enabled && host.config.alert_to) {
-				var message =  "<div>" + host.host + ":" + host.port + " is down!." + "</div><br> <div>Reason: " + state.error + "</div>";
-				if(host.config.url){
-					message = message + "<br><div>URL : "+host.config.url+"</div>";
-				}
-				var params = {host : host.host, port : host.port, to : host.config.alert_to, message : message}; 
-			    email.sendEmail(params);
-			  }
+		httpServer.listen(config.app.port, config.app.hostname, function(req,
+				res, next) {
+			console.log('Http server on port : ' + config.app.port);
+			var watchmen = new WatchMen();
+			watchmen.start();
+			
+			watchmen.on('service_error', function(host, state) {
+				  if (state.prev_state.status === 'success' && host.config.enabled && host.config.alert_to) {
+					var message =  "<div>" + host.host + ":" + host.port + " is down!." + "</div><br> <div>Reason: " + state.error + "</div>";
+					if(host.config.url){
+						message = message + "<br><div>URL : "+host.config.url+"</div>";
+					}
+					var params = {host : host.host, port : host.port, to : host.config.alert_to, message : message}; 
+				    email.sendEmail(params);
+				  }
+			});
+			
+			watchmen.on('service_back', function(host, state) {
+				  if (host.config.enabled && host.config.alert_to){
+					var message =  "<div>" + host.host + ":" + host.port + " is back!." + "</div>";
+					if(host.config.url){
+						message = message + "<br><div>URL : "+host.config.url+"</div>";
+					}
+					var params = {host : host.host, port : host.port, to : host.config.alert_to, message : message};
+				    email.sendEmail(params);
+				  }
+			});
+			
+			watchmen.on('service_warning', function(host, state) {
+				  if (host.config.enabled && host.config.alert_to){
+					var message =  "<div>WARNING : " + host.host + ":" + host.port + " taking more time to respond." + "</div>";
+					if(host.config.url){
+						message = message + "<br><div>URL : "+host.config.url+"</div>";
+					}
+					var params = {host : host.host, port : host.port, to : host.config.alert_to, message : message};
+				    email.sendEmail(params);
+				  }
+			});
+			
 		});
-		
-		watchmen.on('service_back', function(host, state) {
-			  if (host.config.enabled && host.config.alert_to){
-				var message =  "<div>" + host.host + ":" + host.port + " is back!." + "</div>";
-				if(host.config.url){
-					message = message + "<br><div>URL : "+host.config.url+"</div>";
-				}
-				var params = {host : host.host, port : host.port, to : host.config.alert_to, message : message};
-			    email.sendEmail(params);
-			  }
-		});
-		
-		watchmen.on('service_warning', function(host, state) {
-			  if (host.config.enabled && host.config.alert_to){
-				var message =  "<div>WARNING : " + host.host + ":" + host.port + " taking more time to respond." + "</div>";
-				if(host.config.url){
-					message = message + "<br><div>URL : "+host.config.url+"</div>";
-				}
-				var params = {host : host.host, port : host.port, to : host.config.alert_to, message : message};
-			    email.sendEmail(params);
-			  }
-		});
-		
-	});
+	} else {
+		console.log('Error in opening connection with database');
+	}
 });
 
 process.on('uncaughtException', function (err) {
